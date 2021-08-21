2a44b8c2131f13e7597a29359bbb4df17ed04e83
Batman2532 <charjan44@gmail.com>
2021-08-21 08:11:21 +0530

    feat backend added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Login from "./pages/login";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from "./pages/dashboard"
+//import Login from "./pages/login";
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//import Dashboard from "./pages/dashboard"
+import Demo from "./pages/demo";
 function App() {
   return (
-    <BrowserRouter>
-    <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/login" component={Login} />
-      </Switch>
-      <Route path="/dashboard" component={Dashboard} />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Switch>
+    //     <Route exact path="/" component={Login} />
+    //     <Route path="/login" component={Login} />
+    //     <Route path="/dashboard" component={Dashboard} />
+    //   </Switch>
+    // </BrowserRouter>
+     <Demo/>
   );
 }
 
